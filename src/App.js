7fdbe0a32c50e399da40a7677dcb628c044cd1c3
Micro914 <75957513+Micro914@users.noreply.github.com>
2022-01-25 12:52:21 -0700

    Codanai website home router changed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import './App.css';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
-import Home from './pages';
-import SigninPage from './pages/proyecto1';
-import AvisoDePrivacidad from './pages/AvisoDePrivacidad/index';
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./pages";
+import SigninPage from "./pages/proyecto1";
+import AvisoDePrivacidad from "./pages/AvisoDePrivacidad/index";
 
 function App() {
   return (
-    <Router >
+    <Router>
       <Switch>
-        <Route path="/" component={Home} exact/>
-        <Route path="/proyecto1" component={SigninPage} exact/>
-        <Route path="/aviso_privacidad" component={AvisoDePrivacidad} exact/>
+        <Route path="/codanai-website" component={Home} exact />
+        <Route path="/proyecto1" component={SigninPage} exact />
+        <Route path="/aviso_privacidad" component={AvisoDePrivacidad} exact />
       </Switch>
     </Router>
   );
